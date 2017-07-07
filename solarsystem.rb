fce217fbe25d5ed426123ea5d3c205bee3cc774e
AlB <albert.burc@gmail.com>
2017-07-07 14:52:35 -0400

    getting through exercise3

--- solarsystem.rb
+++ solarsystem.rb
@@ -1,13 +1,45 @@
 class System
 
-  def initialize
-    @bodies = []
-  end
 
-  def add(body)
-    @bodies << body
+  @@bodies = []
+
+  def bodies
+    @@bodies
+
+
+  def add
+    @@bodies << self
   end
 
   def total_mass
     sum = 0
     @bodies.each do |one|
+
+    end
+  end
+
+end
+
+class Body < System
+
+  attr_reader :name, :mass
+
+  def initialize(name, mass)
+    @name = name
+    @mass = mass
+  end
+end
+
+class Planets < Body
+
+  def initialize(name, mass, day, year)
+    super (name, mass)
+    @day = day
+    @year = year
+  end
+
+end
+
+class Stars < Body
+
+  def initialize(name, mass, type)
