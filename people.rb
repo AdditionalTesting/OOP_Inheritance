00376451def6daf2f05edc43f3ca778ba2823185
AlB <albert.burc@gmail.com>
2017-07-07 13:48:14 -0400

    finished step 4

--- people.rb
+++ people.rb
@@ -1,13 +1,31 @@
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
 class Student
 
-def learn
-  puts "I get it"
-end
+  def initialize
+
+  end
+
+  def learn
+    puts "I get it"
+  end
 
 end
 
 class Instructor
 
+  def initialize
+
+  end
+
 def teach
   puts "everything in Ruby is an Object"
+end
+
 end
